9f90d96476d7977103a794e97757b89219c0f7c0
enockuwumukiza <148784055+enockuwumukiza@users.noreply.github.com>
2025-08-05 15:24:11 +0000

    added reauth

--- src/store/slices/apiSlice.ts
+++ src/store/slices/apiSlice.ts
@@ -1,14 +1,25 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { clearCredentials } from './userSlice';
 
 const baseQuery = fetchBaseQuery({
   baseUrl: import.meta.env.DEV
     ? '/api' // Use proxy for local development
     : 'https://e-chat-backend-sevs.onrender.com/api', // Use full backend URL in production
-  credentials: 'include' 
+  credentials: 'include'
 });
 
+const baseQueryWithReauth = async (args: any, api: any, extraOptions: any) => {
+  const result = await baseQuery(args, api, extraOptions);
+
+  if (result.error && result.error.status === 401) {
+    api.dispatch(clearCredentials());
+  }
+
+  return result;
+};
+
 export const apiSlice = createApi({
-  baseQuery,
+  baseQuery: baseQueryWithReauth,
   reducerPath: 'api',
   tagTypes: ['User', 'Groups', 'Messages', 'GroupMembers', 'GroupMessages'],
   endpoints: () => ({})
