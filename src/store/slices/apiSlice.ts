1144fb4046a309e2752f7e27e89c944384f1ac00
enockuwumukiza <148784055+enockuwumukiza@users.noreply.github.com>
2025-08-05 14:05:04 +0000

    added backend

--- src/store/slices/apiSlice.ts
+++ src/store/slices/apiSlice.ts
@@ -1,10 +1,15 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const baseQuery = fetchBaseQuery({ baseUrl: '/api' });
+const baseQuery = fetchBaseQuery({
+  baseUrl: import.meta.env.DEV
+    ? '/api' // Use proxy for local development
+    : 'https://e-chat-backend-sevs.onrender.com/api', // Use full backend URL in production
+  credentials: 'include' // if using cookies/auth
+});
 
 export const apiSlice = createApi({
-    baseQuery,
-    reducerPath: 'api',
-    tagTypes: ['User','Groups', 'Messages','GroupMembers', 'GroupMessages'],
-    endpoints: () => ({})
+  baseQuery,
+  reducerPath: 'api',
+  tagTypes: ['User', 'Groups', 'Messages', 'GroupMembers', 'GroupMessages'],
+  endpoints: () => ({})
 });
